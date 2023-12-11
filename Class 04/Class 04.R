e35270de487fab949fb097115284bd53194171f5
Yu-Chia Huang <yuh110@ucsd.edu>
2023-12-10 23:56:37 -0800

    Upload folder

---
+++ Class 04/Class 04.R
@@ -0,0 +1,11 @@
+# This is a text file
+x <- 1:50
+x
+
+plot(x)
+plot(x, sin(x))
+plot(x, sin(x), type="l")
+plot(x, sin(x), type="l", col="blue")
+plot(x, sin(x), type="l", col="blue", lwd=3)
+plot(x, sin(x), type="l", col="coral1", lwd=7)
+plot(x, sin(x), type="l", col="darkturquoise", lwd=5)
